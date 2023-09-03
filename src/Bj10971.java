ceb3f40668cf9eb1678d6ed45e33dec6102a8726
박창준 <qmak01@gmail.com>
2023-09-03 20:24:26 +0900

    230903 HOME

--- src/Bj10971.java
+++ src/Bj10971.java
@@ -1,16 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
+// 외판원 순회2
 public class Bj10971 {
+    static int N;
+    static int[][] map;
+    static int[] isVisited;
+    static int min;
+
+    static void salesman(int dep,int start,int city,int cost){
+        if(dep == N-1){
+            // 모든 도시를 방문 했을 때 출발지로 갈 도로가 없으면 종료.
+            if(map[city][start] == 0) return;
+
+            // 출발지로 가는 비용 합해서 최소면 갱신.
+            cost += map[city][start];
+            if(cost < min) min = cost;
+            return;
+        }
+
+        //dfs
+        for(int i=0;i<N;i++){
+            if(isVisited[i]==0 && map[city][i] != 0){
+                isVisited[i] = 1;
+                salesman(dep+1,start,i,cost+map[city][i]);
+                isVisited[i] = 0;
+            }
+        }
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int[][] map = new int[N][N];
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        isVisited = new int[N];
+        min = Integer.MAX_VALUE;
 
         // 입력.
         for(int i=0;i<N;i++){
@@ -20,41 +48,13 @@ public class Bj10971 {
             }
         }
 
-        ArrayList<Integer> al = new ArrayList();
-
-        al.add(0);
-        int expense = 0;
-
-        while (al.size()!=N){
-            int size = al.size();
-            int minx = -1;
-            int miny = -1;
-            int min = Integer.MAX_VALUE;
-
-            for(int i=0;i<size;i++){
-                int now = al.get(i);
-                for(int j=0;j<N;j++){
-                    if(al.contains(j)) continue;
-                    if(map[now][j]==0) continue;
-                    if(map[now][j] < min){
-                        min = map[now][j];
-                        minx = now;
-                        miny = j;
-                    }
-                    if(map[j][now] < min){
-                        min = map[j][now];
-                        minx = now;
-                        miny = j;
-                    }
-
-                }
-            }
-            al.add(miny);
-            expense+=min;
+        // 출발지를 모두 체크. dfs.
+        for(int i=0;i<N;i++){
+            Arrays.fill(isVisited,0);
+            isVisited[i] = 1;
+            salesman(0,i,i,0);
         }
 
-        System.out.println(expense);
-
-
+        System.out.println(min);
     }
 }
