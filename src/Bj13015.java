51d7ff31ca629f8e92d200256b69bb55cf469bb6
박창준 <qmak01@naver.com>
2024-02-19 15:18:07 +0900

    240219 SSAFY

---
+++ src/Bj13015.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+// 별 찍기 - 23
+
+public class Bj13015 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        System.out.printf("%d",N);
+
+    }
+}
