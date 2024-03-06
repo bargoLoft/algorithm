c7541270dc9904a0e2b54968f7cdfb493e4c5cfd
박창준 <qmak01@naver.com>
2024-03-06 12:58:22 +0900

    240306 SSAFY

--- src/Bj1781.java
+++ src/Bj1781.java
@@ -2,8 +2,9 @@ import java.io.*;
 import java.util.*;
 
 // 컵라면
-// 거꾸로 가면서 해당 데드라인에 푸는 문제 더하기.
-// 한 번에 넣으면 안 되고, 날짜에 해당하는 문제만 넣으면서 하나씩 빼준다.
+// 데드라인 적은 순서로 정렬하고.
+// 넣었을 때 현재 데드라인 날짜보다 큐의 크기가 커지면(3일인데 4문제를 풀 수 없다)
+// 가장 작은 컵라면 수를 뺀다.
 
 public class Bj1781 {
     static class Problem implements Comparable<Problem>{
@@ -17,8 +18,11 @@ public class Bj1781 {
 
         @Override
         public int compareTo(Problem problem){
-           if(this.deadline < problem.deadline) return -1; // 데드라인 높은 순서
-           else return 1;
+            // 데드라인 오름차순.
+           if(this.deadline < problem.deadline) return -1;
+           else if(this.deadline > problem.deadline) return 1;
+           else return 0;
+           // return this.deadline - problem.deadline; 과 동일.
         }
     }
 
@@ -40,14 +44,18 @@ public class Bj1781 {
             problems.add(new Problem(deadline, cup));
         }
 
-        Collections.sort(problems);
+        Collections.sort(problems); // 데드라인 작은 순서로 정렬.
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(Problem problem : problems){
             pq.add(problem.cup);
+            // 현재 데드라인 날짜보다 많은 문제들이 있으면 하나 빼고 간다.
             if(pq.size()> problem.deadline){
-                pq.poll();
+                if(!pq.isEmpty()){
+                    pq.poll();
+                }
+
             }
         }
 
